db9bf99fedbe1bc86c805ff8ceb694d2f00fc7cc
colecornford <cole.cornford@gmail.com>
2020-12-01 17:59:19 +1100

    day1, 10 minutes, n^2 p1 N^3 p2

--- Day1/Day1.go
+++ Day1/Day1.go
@@ -3,7 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"strings"
-	//"strconv"
+	"strconv"
 	"fmt"
 )
 
@@ -22,9 +22,53 @@ func getData()(lines []string){
 
 func part1() {
 	data := getData()
-	fmt.Print(data)
+	//fmt.Print(data)
+	part1:
+	for _, num1 := range data {
+        for _, num2 := range data {
+			x, err := strconv.Atoi(num1)
+			if err != nil {
+
+			}
+			y, err := strconv.Atoi(num2)
+			if err != nil {
+				
+			}
+			if x + y == 2020 {
+				product := x * y
+				fmt.Printf("%d * %d = %d\n", x, y, product)
+				break part1
+			}
+		}
+    }
+
 }
 
 func part2() {
-	// data := getData()
+	data := getData()
+	//fmt.Print(data)
+	part2:
+	for _, num1 := range data {
+        for _, num2 := range data {
+			for _, num3 := range data {
+				x, err := strconv.Atoi(num1)
+				if err != nil {
+
+				}
+				y, err := strconv.Atoi(num2)
+				if err != nil {
+					
+				}
+				z, err := strconv.Atoi(num3)
+				if err != nil {
+					
+				}
+				if x + y + z == 2020 {
+					product := x * y * z
+					fmt.Printf("%d * %d * %d = %d\n", x, y, z, product)
+					break part2
+				}
+			}
+		}
+    }
 }
